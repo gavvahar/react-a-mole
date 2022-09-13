dd6787649ec46a344fc95c71f3aacecdefaece92
gavvahar <nihargavva@gmail.com>
2022-09-12 22:51:26 -0400

    useEffect

--- src/Components/Mole.js
+++ src/Components/Mole.js
@@ -1,3 +1,13 @@
-const Mole = (props) => {};
+import { useEffect } from "react";
+
+const Mole = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.cell(Math.random() * 10000);
+    let timer = setTimeout(() => {
+      props.toggle(false);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  });
+};
 
 export default Mole;
